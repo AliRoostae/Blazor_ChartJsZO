ecc9a34aec05d3677e7fdf5696229c2ed0cde19d
Mohammad <NGMPC4@DESKTOP-DQ991EM>
2022-10-20 17:04:37 +0330

    fix

---
+++ Blazor_ChartJs_Sample/TestData/TestData.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Blazor_ChartJs_Sample.TestData
+{
+    public class TestData
+    {
+        static Random rnd = new Random();
+        static int _min = 10;
+        static int _max = 1000;
+        public List<string> LabelMonth = DateTimeFormatInfo.CurrentInfo.MonthNames.ToList();
+        public List<string> DataForMonth = Enumerable.Repeat(0, 12).Select(i => rnd.Next(_min, _max).ToString()).ToList();
+    }
+}
