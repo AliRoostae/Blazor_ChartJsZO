fb662e5f4d48bfc62486e889ecb357d1fe22a3a2
Mohammad <NGMPC4@DESKTOP-DQ991EM>
2022-10-26 13:18:49 +0330

    add two chart, set responsive UI in project

--- ChartJsStructure/Hellper/Options.cs
+++ ChartJsStructure/Hellper/Options.cs
@@ -3,75 +3,81 @@
 namespace ChartJsStructure.Hellper
 {
     public partial class Options
-        {
-            [JsonProperty("pointBackgroundColor")]
-            public string? PointBackgroundColor { get; set; }
-            [JsonProperty("devicePixelRatio")]
-            public int? DevicePixelRatio { get; set; }
+    {
+        [JsonProperty("pointBackgroundColor")]
+        public string? PointBackgroundColor { get; set; }
+        [JsonProperty("devicePixelRatio")]
+        public int? DevicePixelRatio { get; set; }
 
-            [JsonProperty("fill")]
-            public bool? Fill { get; set; }
+        [JsonProperty("fill")]
+        public bool? Fill { get; set; }
 
-            [JsonProperty("radius")]
-            public int? Radius { get; set; }
+        [JsonProperty("radius")]
+        public int? Radius { get; set; }
 
-            [JsonProperty("indexAxis")]
-            public string? IndexAxis { get; set; }
+        [JsonProperty("indexAxis")]
+        public string? IndexAxis { get; set; }
 
-            [JsonProperty("responsive")]
-            public bool? Responsive { get; set; }
+        [JsonProperty("responsive")]
+        public bool? Responsive { get; set; }
 
-            [JsonProperty("maintainAspectRatio")]
-            public bool? MaintainAspectRatio { get; set; }
+        [JsonProperty("maintainAspectRatio")]
+        public bool? MaintainAspectRatio { get; set; }
 
-            [JsonProperty("aspectRatio")]
-            public int? AspectRatio { get; set; }
-            [JsonProperty("resizeDelay")]
-            public int? ResizeDelay { get; set; }
+        [JsonProperty("aspectRatio")]
+        public int? AspectRatio { get; set; }
+        [JsonProperty("resizeDelay")]
+        public int? ResizeDelay { get; set; }
 
 
 
-            [JsonProperty("showLine")]
-            public bool? ShowLine { get; set; }
+        [JsonProperty("showLine")]
+        public bool? ShowLine { get; set; }
 
-            [JsonProperty("stacked")]
-            public bool? Stacked { get; set; }
+        [JsonProperty("stacked")]
+        public bool? Stacked { get; set; }
 
-            [JsonProperty("animation")]
-            public OptionsAnimation? Animation { get; set; }
+        [JsonProperty("animation")]
+        public OptionsAnimation? Animation { get; set; }
 
-            [JsonProperty("animations")]
-            public OptionsAnimations? Animations { get; set; }
+        [JsonProperty("animations")]
+        public OptionsAnimations? Animations { get; set; }
 
-            [JsonProperty("interaction")]
-            public Interaction? Interaction { get; set; }
+        [JsonProperty("hoverRadius")]
+        public int? HoverRadius { get; set; }
 
-            [JsonProperty("datasets")]
-            public Datasets? Datasets { get; set; }
+        [JsonProperty(" hoverBackgroundColor")]
+        public string? HoverBackgroundColor { get; set; }
 
-            [JsonProperty("elements")]
-            public Elements? Elements { get; set; }
+        [JsonProperty("interaction")]
+        public Interaction? Interaction { get; set; }
 
-            [JsonProperty("layout")]
-            public Layout? Layout { get; set; }
+        [JsonProperty("datasets")]
+        public Datasets? Datasets { get; set; }
 
-            [JsonProperty("plugins")]
-            public Plugins? Plugins { get; set; }
+        [JsonProperty("elements")]
+        public Elements? Elements { get; set; }
 
-            [JsonProperty("scales")]
-            public Scales? Scales { get; set; }
+        [JsonProperty("layout")]
+        public Layout? Layout { get; set; }
 
-            [JsonProperty("transitions")]
-            public Transitions? Transitions { get; set; }
+        [JsonProperty("plugins")]
+        public Plugins? Plugins { get; set; }
+
+        [JsonProperty("scales")]
+        public Scales? Scales { get; set; }
+
+        [JsonProperty("transitions")]
+        public Transitions? Transitions { get; set; }
 
         [JsonProperty("onClick")]
-        public string OnClick => ""; 
+        public string OnClick => "";
         [JsonProperty("events")]
-        public string[] Events => new[] { "click" } ;
-        }
-
-     
+        public string[] Events => new[] { "click" };
     }
+
+
+}
 
 
 
